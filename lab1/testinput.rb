6d77899849a4cb903afdaadee72a18879bd3e286
jacobjwoods <jacobjwoods05@gmail.com>
2020-01-30 18:26:15 -0500

    testinput.rb

---
+++ lab1/testinput.rb
@@ -0,0 +1,71 @@
+#define class test
+# created by Jacob Woods
+class Test
+  def initialize
+
+    @card = []
+  end
+
+
+# enter the three locations of the cards the user wants to try and the 12 card array
+def check_input(var1, var2, var3 , delt)
+
+  #add user designated card to 3 card check array
+  @card[0] = delt[var1]
+  @card[1]= delt[var2]
+  @card[2] = delt[var3]
+
+  #set check to true
+  check = true
+  i = 0
+  #while loop iterates through each specification of card (number first then color etc)
+	while i < 4 && check == true do
+    #check every card that it doesnt have a double
+		for j in 0..2 do
+      #use count to keep track of double
+      count = 0
+      #set strings to compare if equal
+      t = @card[j][i].to_s
+      s = @card[0][i].to_s
+
+      #compare for each of the 3 cards
+			if (t <=> s) == 0
+        #add to count if they are the same
+        count = count + 1
+
+			end
+
+      s = @card[1][i].to_s
+			if (t <=> s) == 0
+         count = count + 1
+
+			end
+      s = @card[2][i].to_s
+      if (t <=> s) == 0
+        count = count + 1
+      end
+      #check for every loop if there is a double then set check
+      if count == 2
+        check = false
+
+      end
+
+		end
+		
+		
+		 if count == 2
+        check = false
+
+     end
+    #iteration
+    i = i + 1
+	end
+  #return check
+check
+
+end
+
+end
+
+			
+
