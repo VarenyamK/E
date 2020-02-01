fd3f8303d07fdcb61e69051521df4506138174db
Nick Connors <connors.131@osu.edu>
2020-01-31 21:13:07 -0500

    Added method to String class to print with color and added those methods to the print_card method. Fixed deal method to deal out 12 cards at the start and 3 cards every time called after. Removed print statements printing out each individual card in the hand and added display_hand method that prints the cards out in the hand. Finally, fixed some minor formatting issues.

---
+++ lab1/Cards.rb
@@ -0,0 +1,94 @@
+# Author @Varenyam Kaushik, Nick Connors
+
+# define class Card
+class Card
+  attr_accessor(:number, :color, :shape, :shading)
+
+  def initialize(number, color, shape, shading)
+    # defines attributes of a card
+    @number = number
+    @color = color
+    @shape = shape
+    @shading = shading
+  end
+
+  # allows to print out card values
+  def print_card
+    card = "#{@number} #{@color} #{@shape} #{@shading}"
+    if @color == 'red'
+      puts card.red
+    elsif @color == 'blue'
+      puts card.blue
+    elsif @color == 'green'
+      puts card.green
+    end
+  end
+end
+
+# Define class Deck
+class Deck
+  def initialize
+    # defines the allowed parameter values for deck
+    @numbers = [1, 2, 3]
+    @colors = %w[red green blue]
+    @shapes = %w[diamond oval squiggle]
+    @shadings = %w[hollow shaded filled]
+
+    # initialize array that stores cards in the deck
+    @cards = []
+
+    # initializes the deck of cards in an array using for each loops
+    @numbers.each do |number|
+      @colors.each do |color|
+        @shapes.each do |shape|
+          @shadings.each do |shading|
+            @cards << Card.new(number, color, shape, shading)
+          end
+        end
+      end
+    end
+    # shuffles cards deck
+    @cards.shuffle!
+  end
+
+  # deals the cards
+  def deal(hand)
+    # hand is 12 to start
+    if hand.length.zero?
+      12.times { hand.push(@cards.shift) }
+    else
+      # if match found or not, add 3 to hand
+      3.times { hand.push(@cards.shift) }
+    end
+  end
+
+  # provides the number of cards left for debugging purposes
+  def cards_left
+    @cards.length
+  end
+end
+
+def display_hand(hand)
+  hand.each(&:print_card)
+end
+
+# Add method to String class to add color to terminal output
+# Credit to Erik Skoglund: https://stackoverflow.com/a/11482430/8760155
+class String
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def blue
+    colorize(34)
+  end
+end
+
--- lab1/Lab1.rb
+++ lab1/Lab1.rb
@@ -1,1 +1,14 @@
-FIle to add ruby code to. 
+# The Game of Set
+require './Cards.rb'
+
+deck = Deck.new
+
+# initialize hand of 12
+hand = []
+deck.deal(hand)
+# Delete Later: All future deals will add 3 cards to the hand
+# Case 1: Match found. Hand size is 9, so add 3 more cards.
+# Case 2: Match not found. Add 3 more cards. Hand size now 15. Repeat dealing 3
+# if necessary
+
+display_hand(hand)
