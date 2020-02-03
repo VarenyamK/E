d7ec6e1012e0f8775c538bb447567d985688945f
Hitesh Bavisetty <Hitesh@Hiteshs-MacBook-Pro.local>
2020-02-03 01:52:23 -0500

    Added a hint. User gets 3 hints per game. Added method that finds every possible comination of 3 cards in the hand of 12 cards, then calls the check_input method to see if any combination is a set. Method breaks if set is found, then prints hint to user. Also edited the user input to take h for a hint.

--- lab1/Lab1.rb
+++ lab1/Lab1.rb
@@ -2,11 +2,33 @@
 require './Cards.rb'
 require './testinput.rb'
 
+
+#method that takes in 12 card hand and prints a hint
+def hint(cards12)
+   arr12 = Array[0,1,2,3,4,5,6,7,8,9,10,11]
+   allSets = Array.new
+   allSets = arr12.combination(3).to_a
+   
+   i = 0
+   while i < allSets.length()
+	checkHint = Test.new
+	comb = Array.new
+	comb = allSets[i]
+	crd1 = comb[0]
+	crd2 = comb[1]
+	crd3 = comb[2]
+   	isSet = checkHint.check_input(crd1,crd2,crd3, cards12)
+	break if isSet
+        i +=1
+   end
+   puts "#{crd1} #{crd2} #{crd3}"		
+end
+
 deck = Deck.new
 check = Test.new
 
 totalTime = 0
-
+hints = 0
 # initialize hand of 12
 hand = []
 deck.deal(hand)
@@ -26,12 +48,27 @@ while deck.cards_left > 3
   timeNow = Time.now
 
   #ask for user input
-  puts 'enter 3 numbers with enter inbetween each to say which cards you want(n if there is not a set and q to quit game): '
-
+  puts 'Enter 3 numbers with enter inbetween each to say which cards you want(n if there is not a set and q to quit game): '
+  puts 'Stuck? press h for a hint. You will only get 3 hints per game so use them wisely'
+  
   #get input from user
   var1 = gets.chomp
+  #if user needs a hind, hint will be given, up to 3 hints per game
+  if (var1 <=> 'h') == 0
+    #makes sure user has hints left
+    if hints < 3    
+      puts 'Hint: the following set of 3 cards is a set'
+      hint(hand)
+    elsif hints >= 3
+      #if user used all 3 hints, no more are available
+      puts 'Sorry, you used all 3 hints this game. Enter 3 cards with enter inbetween each or n if there is no set, q to quit'
+    end
+     hints +=1
+     var1 = gets.chomp
+  end
+  
   #check for quit statement
-  if (var1 <=> 'q') == 0
+   if (var1 <=> 'q') == 0
     #exit game loop
     break
     #check for no set statement
@@ -83,3 +120,5 @@ puts 'total points earned: '
 puts points
 #displays total time taken
 puts "#{totalTime} is the total time taken"
+
+
