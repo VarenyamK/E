8330fc977178bbace7e5725dfaf83ce2e147f19e
Connor Babcock <cbabcock7@gmail.com>
2020-02-04 08:27:33 -0500

    added play again and multiple players that store name and sets. Added a winner based on total sets.

--- lab1/Lab1.rb
+++ lab1/Lab1.rb
@@ -1,6 +1,7 @@
 # The Game of Set
 require './Cards.rb'
 require './testinput.rb'
+require './Players.rb'
 
 #method to check for duplicate inputs
 def checkduplicate(var1, var2, var3)
@@ -50,6 +51,23 @@ def checkbounds (var1, var2 , var3, arr)
   tf
 end
 
+# gets a valid player id and returns it
+def check_player_id(players_arr)
+  check = false
+  puts 'Enter player number: '
+  id = gets.chomp
+  while !check
+    if (id.to_i > 0 && id.to_i <= players_arr.length().to_i)
+      check = true
+    else
+      puts 'Invalid player number.'
+      puts 'Enter player number: '
+      id = gets.chomp
+    end
+  end
+  id
+end
+
 # method that takes in 12 card hand and prints a hint
 def hint(cards12)
   arr12 = (0..(cards12.length()-1)).to_a
@@ -72,114 +90,167 @@ def hint(cards12)
   puts "#{crd1} #{crd2} #{crd3}"
 end
 
-deck = Deck.new
-check = Test.new
+play_again = true
 
-totalTime = 0
-hints = 0
+# initialize players list
+players = []
 
-# initialize hand of 12
-hand = []
-deck.deal(hand)
+puts 'Enter number of players: '
+num_of_players = gets.to_i
+player_number = 1
 
-# get to point counter
-points = 0
-
-# stay playing until out of cards to deal
-while deck.cards_left > 3
-
-  # gets current time
-  timeNow = Time.now
-  #get variables for the user checks
-  checkb = false
-  checkd = false
-  # while loop to loop till correct inputs
-  while (checkb == false) || (checkd == false)
-
-    # print the dealt hand
-    display_hand(hand)
-
-    # ask for user input
-    puts 'Enter 3 numbers with enter inbetween each to say which cards you want(n if there is not a set and q to quit game): '
-    puts 'Stuck? press h for a hint. You will only get 3 hints per game so use them wisely'
-
-    # get input from user
-    var1 = gets.chomp
-    # if user needs a hind, hint will be given, up to 3 hints per game
-    unless (var1 <=> 'h') != 0
-      # makes sure user has hints left
-      if hints < 3
-        puts 'Hint: the following set of 3 cards is a set'
-        hint(hand)
-      elsif hints >= 3
-        #if user used all 3 hints, no more are available
-        puts 'Sorry, you used all 3 hints this game. Enter 3 cards with enter inbetween each or n if there is no set, q to quit'
-      end
-      hints += 1
-      var1 = gets.chomp
-    end
-
-    # check for quit statement
-    if (var1 <=> 'q').zero?
-      # exit game loop
-      exit!
-      # check for no set statement
-    elsif (var1 <=> 'n').zero?
-      # re-deal hand
-      deck.deal(hand)
-      # skip rest of loop
-      next
-    end
-
-    # get variables
-    var1 = var1.to_i
-    var2 = gets.to_i
-    var3 = gets.to_i
-    # check if user put duplicates which is not allowed
-    checkd = checkduplicate(var1,var2,var3)
-    # if statement to keep from checking the same numbers (will return true if this happens)
-    if checkd == true
-      # check if variables are out of range
-      checkb = checkbounds(var1, var2, var3, hand)
-    end
-
-  end
-  # calculates time taken for user to respond
-  timeAfter = Time.now
-  timeTaken = timeAfter - timeNow
-
-  # check the user input to see if it is a set
-  checkuser = check.check_input(var1, var2, var3, hand)
-  # display if it is a set
-  puts checkuser
-
-  # if it is a set remove the cards from the deck and give user a point
-  next unless checkuser
-
-  # displays time taken to find set
-  puts "#{timeTaken} seconds taken to find a set"
-  totalTime += timeTaken
-
-  str1 = hand[var1]
-  str2 = hand[var2]
-  str3 = hand[var3]
-
-  hand.delete(str1)
-  hand.delete(str2)
-  hand.delete(str3)
-
-  #if the array has been added to (by there not being a set) do not add more cards
-  if (hand.length() < 12)
-    # adding cards to hand
-    puts ' adding cards'
-    deck.deal(hand)
-  end
-  # add points
-  points += 1
+# adds players to the players list
+while player_number <= num_of_players
+  puts 'Enter player ' + player_number.to_s + '\'s name: '
+  name = gets.to_s
+  player = Player.new player_number, name
+  players << player
+  player_number += 1
 end
 
-# display total points
-puts 'total points earned: '
-puts points
-# displays total time taken
-puts "#{totalTime} is the total time taken"
+# keep starting new games until player wants to stop
+while play_again
+
+  deck = Deck.new
+  check = Test.new
+
+  totalTime = 0
+  hints = 0
+  replay = true
+
+  # initialize hand of 12
+  hand = []
+  deck.deal(hand)
+
+  # stay playing until out of cards to deal
+  while deck.cards_left > 3
+
+    current_player = 0
+    # gets current time
+    timeNow = Time.now
+    #get variables for the user checks
+    checkb = false
+    checkd = false
+    # while loop to loop till correct inputs
+    while (checkb == false) || (checkd == false)
+
+      # print the dealt hand
+      display_hand(hand)
+
+      # pulls player from the player list based on the id entered
+      answer_id = check_player_id(players)
+      current_player = players[answer_id.to_i - 1]
+      puts current_player.name + ' is the selected player'
+
+      # ask for user input
+      puts 'Enter 3 numbers with enter inbetween each to say which cards you want(n if there is not a set and q to quit game): '
+      puts 'Stuck? press h for a hint. You will only get 3 hints per game so use them wisely'
+
+      # get input from user
+      var1 = gets.chomp
+      # if user needs a hind, hint will be given, up to 3 hints per game
+      unless (var1 <=> 'h') != 0
+        # makes sure user has hints left
+        if hints < 3
+          puts 'Hint: the following set of 3 cards is a set'
+          hint(hand)
+        elsif hints >= 3
+          #if user used all 3 hints, no more are available
+          puts 'Sorry, you used all 3 hints this game. Enter 3 cards with enter inbetween each or n if there is no set, q to quit'
+        end
+        #hints += 1
+        var1 = gets.chomp
+      end
+
+      # check for quit statement
+      if (var1 <=> 'q').zero?
+        # exit game loop
+        exit!
+        # check for no set statement
+      elsif (var1 <=> 'n').zero?
+        # re-deal hand
+        deck.deal(hand)
+        # skip rest of loop
+        next
+      end
+
+      # get variables
+      var1 = var1.to_i
+      var2 = gets.to_i
+      var3 = gets.to_i
+      # check if user put duplicates which is not allowed
+      checkd = checkduplicate(var1,var2,var3)
+      # if statement to keep from checking the same numbers (will return true if this happens)
+      if checkd == true
+        # check if variables are out of range
+        checkb = checkbounds(var1, var2, var3, hand)
+      end
+
+    end
+    # calculates time taken for user to respond
+    timeAfter = Time.now
+    timeTaken = timeAfter - timeNow
+
+    # check the user input to see if it is a set
+    checkuser = check.check_input(var1, var2, var3, hand)
+    # display if it is a set
+    puts checkuser
+
+    # if it is a set remove the cards from the deck and give user a point
+    next unless checkuser
+
+    # displays time taken to find set
+    puts "#{timeTaken} seconds taken to find a set"
+    totalTime += timeTaken
+
+    str1 = hand[var1]
+    str2 = hand[var2]
+    str3 = hand[var3]
+
+    hand.delete(str1)
+    hand.delete(str2)
+    hand.delete(str3)
+
+    #if the array has been added to (by there not being a set) do not add more cards
+    if (hand.length() < 12)
+      # adding cards to hand
+      puts ' adding cards'
+      deck.deal(hand)
+    end
+    # add points
+    current_player.set += 1
+    current_player.total_sets += 1
+  end
+
+  # display total points
+  players.each do |player|
+    puts player.name
+    print 'Sets gotten this game: ' + player.set.to_s + ', '
+    puts 'Total sets: ' + player.total_sets.to_s
+    player.set = 0
+  end
+
+  # displays total time taken
+  puts "#{totalTime} is the total time taken"
+
+  # asks user if they want to play again
+  puts 'Enter \'1\' to play again: '
+  answer = gets.to_i
+  if answer != 1
+    play_again = false
+    puts 'Thank you for playing!'
+  end
+end
+
+winner = ' '
+winner_sets = 0
+
+# gets winner who has the most sets
+players.each do |player|
+  if player.total_sets.to_i > winner_sets.to_i
+    winner = player.name
+    winner_sets = player.total_sets
+  end
+end
+
+puts 'The winner is ' + winner.to_s + ' with ' + winner_sets.to_s + ' sets!'
