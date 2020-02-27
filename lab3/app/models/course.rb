bd9279db5a639d998b6d3230aa5f1f9c7d292c96
Varenyam Kaushik <kaushik.35@osu.edu>
2020-02-27 15:32:44 -0500

    Fixed formatting on the edit and add course page. Also added an add button to the top

--- lab3/app/models/course.rb
+++ lab3/app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
   # validates that the section is a number
-  validates :section, numericality:  {only_integer: true, message: "Section number must be a number." }
+  validates :section, numericality: { only_integer: true, message: ' must be a number.' }
   # validates that the component is either lec, lab, or ind
-  validates :component, inclusion: {in: %w(LEC LAB IND ), message: "The component must be LEC, LAB or IND."}
+  validates :component, inclusion: { in: %w(LEC LAB IND), message: ' must be LEC, LAB or IND.' }
 end
