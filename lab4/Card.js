98b8ed38292c69c3057c28f32ca72ecce0b3480a
Varenyam Kaushik <kaushik.35@osu.edu>
2020-03-12 01:53:22 -0400

    Added comments to Card and Deck class.

--- lab4/Card.js
+++ lab4/Card.js
@@ -5,5 +5,7 @@ class Card{
         this.color = color;
         this.shape = shape;
         this.shading = shading;
+        //For adding image path/link to the object
+        //this.pic = pic;
     }
 }
--- lab4/Deck.js
+++ lab4/Deck.js
@@ -1,16 +1,19 @@
 //Define deck object
 class Deck {
     constructor() {
-
+        //Create the deck array that will hold the cards.
         this.deck = [];
+        //Define the attributes of a card
         const numbers = [1, 2, 3];
         const colors = ['red', 'green', 'blue'];
         const shapes = ['diamond', 'oval', 'squiggle'];
         const shadings = ['hollow', 'shaded', 'filled'];
+        //Initialize the deck of cards in the deck array using for each loops
         for (let number in numbers) {
             for (let color in colors) {
                 for (let shape in shapes) {
                     for (let shading in shadings) {
+                        //add the values from the array to the card object
                         const card = new Card(`${numbers[number]}`, `${colors[color]}`, `${shapes[shape]}` , `${shadings[shading]}`);
                         this.deck.push(card);
                     }
@@ -19,7 +22,8 @@ class Deck {
         }
     }
 
-
+    //Source: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    //Implementation of the Fisher-Yates Shuffle to shuffle the deck
     shuffle() {
         const {deck} = this;
         let m = deck.length, i;
@@ -31,6 +35,7 @@ class Deck {
         return this;
     }
 
+    //pop and return a card from the deck
     deal() {
         return this.deck.pop();
     }
