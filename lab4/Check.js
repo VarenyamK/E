c17b62dfa3fdf6f262b69ed471a84ca4f89b3477
jacobjwoods <jacobjwoods05@gmail.com>
2020-03-14 17:50:46 -0400

    added Check class to check if any three cards is a set and added the check.js to html

---
+++ lab4/Check.js
@@ -0,0 +1,102 @@
+class Check{
+    constructor(){
+        //declare global
+        this.cards = []
+    }
+
+    check(var1, var2, var3, delt){
+        //declare variables
+        let s;
+        let count;
+        let t;
+        this.cards[0] = delt[var1];
+        this.cards[1] = delt[var2];
+        this.cards[2] = delt[var3];
+
+        let check = true;
+        //go through each number section of the 3 cards and compare them with each other
+        for(let i = 0; i < this.cards.length; i++) {
+            count = 0;
+            //let t and s be the two elements to be compared to
+            t = this.cards[i].number;
+
+            for (let j = 0; j < this.cards.length; j++) {
+
+                s = this.cards[j].number;
+
+                if (t === s) {
+                    count++;
+                }
+
+            }
+
+            if (count === 2){
+                check =  false;
+            }
+
+        }
+        //go through each color section of the 3 cards and compare them with each other
+        for(let i = 0; i < this.cards.length; i++) {
+            count = 0;
+
+            t = this.cards[i].color;
+
+            for (let j = 0; j < this.cards.length; j++) {
+
+                s = this.cards[j].color;
+
+                if (t === s) {
+                    count++;
+                }
+
+            }
+            //if at anytime there are two of the same type then it is not a match
+            if (count === 2){
+                check =  false;
+            }
+        }
+        //go through each shape section of the 3 cards and compare them with each other
+        for(let i = 0; i < this.cards.length; i++) {
+            count = 0;
+
+            t = this.cards[i].shape;
+
+            for (let j = 0; j < this.cards.length; j++) {
+
+                s = this.cards[j].shape;
+
+                if (t === s) {
+                    count++;
+                }
+
+
+            }
+            //if at anytime there are two of the same type then it is not a match
+            if (count === 2){
+                check =  false;
+            }
+        }
+        //go through each shading section of the 3 cards and compare them with each other
+        for(let i = 0; i < this.cards.length; i++) {
+            count = 0;
+
+            t = this.cards[i].shading;
+
+            for (let j = 0; j < this.cards.length; j++) {
+
+                s = this.cards[j].shading;
+
+                if (t === s) {
+                    count++;
+                }
+
+            }
+            //if at anytime there are two of the same type then it is not a match
+            if (count === 2){
+                check =  false;
+            }
+        }
+        //return treu or false
+        return check;
+    }
+}
